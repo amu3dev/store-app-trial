ee662ff847cab97ea09c7d8dba274d55dc103aa2
Ahmed Yosry <amu3dev@gmail.com>
2023-02-02 20:43:13 +0200

    - Added <Link>

--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -1,11 +1,14 @@
 import Like from "./Like";
+import { Link } from "react-router-dom";
 
 const TableBody = ({ items, onLike, onDelete }) => {
   return (
     <tbody>
       {items.map((item) => (
         <tr key={item._id}>
-          <td>{item.title}</td>
+          <td>
+            <Link to={item._id}> {item.title}</Link>
+          </td>
           <td>{item.genre.name}</td>
           <td>{item.numberInStock}</td>
           <td>{item.dailyRentalRate}</td>
