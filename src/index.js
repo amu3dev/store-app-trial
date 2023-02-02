36d3d626a911bb6bc8e74d11685e0cbecdc1fba5
Ahmed Yosry <amu3dev@gmail.com>
2023-02-02 20:46:42 +0200

    - Added new structure including  <BrowserRouter>, <Routes> and <Route> - Added   <TableNavbar /> - Added some extra components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TableNavbar from "./components/TableNavbar";
+import Customers from "./components/Customers";
+import Rentals from "./components/Rentals";
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
@@ -8,14 +12,28 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./index.css";
 
 import reportWebVitals from "./reportWebVitals";
-import App from "./components/App";
+// import App from "./components/App";
 import Board from "./components/Board";
+import NotFound from "./components/NotFound";
+import MoviesDetails from "./components/MoviesDetails";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Board />
-    <App />
+    <BrowserRouter>
+      <TableNavbar />
+      <Routes>
+        <Route path="/" element={<Board />} />
+        <Route path="/movies/:id" element={<MoviesDetails />} />
+
+        <Route path="/movies" element={<Board />} />
+        <Route path="/customers" element={<Customers />} />
+        <Route path="/rentals" element={<Rentals />} />
+        <Route path="/not-found" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      {/* <App /> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
 
